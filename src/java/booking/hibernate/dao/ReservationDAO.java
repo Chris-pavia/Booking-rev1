7ec9f52aa39b59b007a9e186c076ff681b3ea344
stagiaire <stagiaire@192.168.1.81>
2013-05-23 11:26:14 +0200

    css du fragment central

---
+++ src/java/booking/hibernate/dao/ReservationDAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package booking.hibernate.dao;
+
+import org.hibernate.Session;
+import booking.hibernate.dao.HibernateUtil;
+import booking.hibernate.entities.Reservation;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+
+public class ReservationDAO {
+    private Session session = null;
+    
+    // -------------
+    public ReservationDAO() {
+        this.session = HibernateUtil.getSessionFactory().openSession();
+    } /// constructeur
+    
+    public List<Reservation> getReservation(int nb) {
+        // ----------------------------
+        List<Reservation> res = null;
+
+        try {
+            // --- FROM NomDeClasse
+            Query q = this.session.createQuery("FROM Reservation WHERE id_art=1");
+            
+            res = (List<Reservation>) q.list();
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+
+        return res;
+    }/// select
+}
